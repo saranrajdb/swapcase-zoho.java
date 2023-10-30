5aea1d56e9fa5b8526662b03a2630f1c580e0517
saranrajdb <110327536+saranrajdb@users.noreply.github.com>
2023-10-30 18:55:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str=sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            int d=(int)str.charAt(i)^32;
+            System.out.print((char)d);
+        }
+    }
+}
